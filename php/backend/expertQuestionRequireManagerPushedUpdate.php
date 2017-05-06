5fc99d5fe493a603e2beb9d853d6afb3c22633bd
liangwenzhu <826415551@qq.com>
2017-05-07 00:57:56 +0800

    专家问答申请功能模块完成

--- php/backend/expertQuestionRequireManagerPushedUpdate.php
+++ php/backend/expertQuestionRequireManagerPushedUpdate.php
@@ -10,7 +10,7 @@ $questionIntroduce = $_POST[questionIntroduce];
 $questionDetailIntroduce = $_POST[questionDetailIntroduce];
 $advantage = $_POST[advantage];
 $questionRequireStatus = $_POST[questionRequireStatus];
-
+$tieziSection = $_POST[tieziSection];
 $expertUpdateSql = "UPDATE Expert
         SET advantage = '$advantage'
         WHERE expertId = '$expertId'";
@@ -18,15 +18,32 @@ $expertUpdateSql = "UPDATE Expert
 if(!mysql_query($expertUpdateSql,$con)){
     die('error: ' . mysql_error());
 }
+/*查出专家问答表中的帖子Id。对帖子的内容也进行修改*/
+$tieziIdSelectSql = "select tieziId from expertQuestion where questionRequireId = '$questionRequireId'";
+
+$tieziIdSelectSqlResult = mysql_query($tieziIdSelectSql,$con);
+$tieziIdSelectSqlRow = mysql_fetch_array($tieziIdSelectSqlResult);
+$tieziId = $tieziIdSelectSqlRow['tieziId'];
+/*帖子内容进行更改*/
+$tieziContentUpdateSql = "UPDATE Tiezi
+        SET tieziTitle = '$questionTitle',
+			tieziContent = '$questionIntroduce',
+			tieziSection = '$tieziSection',
+			tieziCreaterData = now()
+        WHERE tieziId = '$tieziId'";
+
+if(!mysql_query($tieziContentUpdateSql,$con)){
+    die('error: ' . mysql_error());
+}
+
+
 $ExpertQuestionRequireUpdateSql = "UPDATE ExpertQuestionRequire
         SET questionTitle = '$questionTitle',
 			questionShowTime = '$questionShowTime',
 			questionIntroduce = '$questionIntroduce',
 			questionDetailIntroduce = '$questionDetailIntroduce',
-			questionEditTime = now(),
-			questionRequireStatus = '$questionRequireStatus'
+			questionEditTime = now()
         WHERE questionRequireId = '$questionRequireId'";
-
 if(!mysql_query($ExpertQuestionRequireUpdateSql,$con)){
     die('error: ' . mysql_error());
 }else{
